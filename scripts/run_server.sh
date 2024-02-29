39cff8c1f0f7347127528d651ba7e365e6e57ea7
Chris Mulder <30597826+muldercw@users.noreply.github.com>
2024-02-28 21:44:31 -0500

    Update run_server.sh

--- scripts/run_server.sh
+++ scripts/run_server.sh
@@ -1,37 +1,32 @@
 #!/bin/bash
+
 # Update server files
-if [ $UPDATE = true ]
-then
+if [ "$UPDATE" = true ]; then
     echo "Checking for server updates..."
-    /usr/games/steamcmd +login anonymous +force_install_dir /server +app_update $STEAM_APP_ID validate +quit
+    /usr/games/steamcmd +login anonymous +force_install_dir /server +app_update "$STEAM_APP_ID" validate +quit
 fi
 
 # Update mods
-if [ $UPDATE_MODS = true ]
-then
+if [ "$UPDATE_MODS" = true ]; then
     echo "Checking for mod updates..."
-    for $m in $MOD_IDS
-    do
-        source /download.sh -w $m
+    for m in $MOD_IDS; do
+        source /download.sh -w "$m"
     done
 fi
 
 # Automatically load all mods
-if [ $AUTO_MODS = true ]
-then
+if [ "$AUTO_MODS" = true ]; then
     echo "Loading all mods..."
-    $MODS_FOUND=""
-    for d in "/server/mods"
-    do
+    MODS_FOUND=""
+    for d in /server/mods/*; do
         echo "Found mod $d"
-        $MODS_FOUND="${MODS_FOUND}mods/$d;"
+        MODS_FOUND="${MODS_FOUND}mods/$d;"
     done
-    MODS=${MODS_FOUND::-1}
+    MODS="${MODS_FOUND::-1}"
 fi
 
 # Debug
-if [ $DEBUG = true ]
-then
+if [ "$DEBUG" = true ]; then
     echo "Debug enabled, system variables:"
     echo "Steam App ID: $STEAM_APP_ID"
     echo "CPU Count: $CPU_COUNT"
@@ -41,8 +36,7 @@ then
 fi
 
 # Logs cleanup
-if [ $DELETE_LOGS = true ]
-then
+if [ "$DELETE_LOGS" = true ]; then
     echo "Removing logs and error dumps..."
     rm -f /server/profile/*.log
     rm -f /server/profile/*.RPT
@@ -52,15 +46,15 @@ then
 fi
 
 # First run init
-if [[ ! -d "./config/myserver/serverDZ.cfg" ]]; then
-    echo "Template folder doesn't exist, creating...";
+if [ ! -d "./config/myserver/serverDZ.cfg" ]; then
+    echo "Template folder doesn't exist, creating..."
     source /init.sh
 fi
 
 # Copy mod keys
 echo "Copying keys..."
-find /server/mods -name '*.bikey' -exec cp -prv '{}' '/server/keys' ';' # | cpio -upm /server/keys
+find /server/mods -name '*.bikey' -exec cp -prv '{}' '/server/keys' ';'
 
 # Run server
 echo "Starting server..."
-/server/DayZServer -cpuCount=$CPU_COUNT -dologs -adminlog -netlog -freezecheck -port="${GAME_PORT}" -profiles="${PROFILE_DIR}" -config="${CONFIG_FILE}" "-mod=${MODS}"
+/server/DayZServer -cpuCount="$CPU_COUNT" -dologs -adminlog -netlog -freezecheck -port="$GAME_PORT" -profiles="$PROFILE_DIR" -config="$CONFIG_FILE" "-mod=$MODS"
